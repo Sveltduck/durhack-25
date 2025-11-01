3160803fa112f2ca2e3fdefcf48aeccc0aa4f54c
hopperelec <hopperelec@gmail.com>
2025-11-01 22:45:37 +0000

    Make list-style "personality" inputs tag-style

---
+++ frontend/src/lib/components/FormTagsInput.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Tags from "svelte-tags-input";
+
+    let { name, placeholder }: {
+        name: string;
+        placeholder: string;
+    }= $props();
+
+    let tags: string[] = $state([]);
+</script>
+
+<input type="hidden" {name} value={JSON.stringify(tags)} />
+<Tags bind:tags {placeholder} allowPaste onlyUnique />
