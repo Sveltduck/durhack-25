eb33fe0fc036725523b4f3e594fc0c64daa12144
hopperelec <hopperelec@gmail.com>
2025-11-02 09:51:38 +0000

    Made the form tags hidden input only *visually* hidden, so validity message is shown

--- frontend/src/lib/components/FormTagsInput.svelte
+++ frontend/src/lib/components/FormTagsInput.svelte
@@ -23,5 +23,20 @@
     onMount(onTagRemoved);
 </script>
 
-<input type="text" hidden {name} value={JSON.stringify(tags)} bind:this={inputElement} />
+<!-- Not hidden with type="hidden" so that validity message is shown -->
+<input type="text" {name} value={JSON.stringify(tags)} bind:this={inputElement} />
 <Tags bind:tags {placeholder} allowPaste onlyUnique {onTagAdded} {onTagRemoved} />
+
+<style>
+/* Hides the element visually but keeps it accessible to browsers and screen readers */
+input {
+    border: 0;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
+}
+</style>
